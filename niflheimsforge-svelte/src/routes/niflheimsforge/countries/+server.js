36d80a4455b416e3573fa1450ba89ffa1409da43
JuicyBram <bvdwallen1912@gmail.com>
2024-05-31 12:24:40 +0200

    Fixed the post request with the parameter mismatch

--- niflheimsforge-svelte/src/routes/niflheimsforge/countries/+server.js
+++ niflheimsforge-svelte/src/routes/niflheimsforge/countries/+server.js
@@ -1,7 +1,7 @@
 import { json } from '@sveltejs/kit';
 
 export async function GET(request) {
-    const response = await request.fetch(`${import.meta.env.VITE_API_ENDPOINT}/countries`);
+    const response = await request.fetch(`http://localhost:5031/countries`);
 
     if (response.ok) {
         const countries = await response.json();
@@ -16,42 +16,23 @@ export async function GET(request) {
     }
 }
 
-// export async function POST(countryDTO) {
-//     const response = await fetch(`http://localhost:5031/countries`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(countryDTO)
-//     });
-
-//     if (response.ok) {
-//         const country = await response.json();
-//         return country;
-//     } else {
-//         console.error(`Failed to create country: ${response.statusText}`);
-//         throw new Error(response.statusText);
-//     }
-// }
-
 export async function POST({ request }) {
-    try {
+    try{
         const countryDTO = await request.json();
-
-        const response = await fetch('http://localhost:5031/countries', {
+        const response = await fetch(`http://localhost:5031/countries`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(countryDTO)
         });
-
+    
         if (response.ok) {
             const country = await response.json();
-            return json(country, { status: 201 });
+            return country;
         } else {
-            const error = await response.json();
-            return json({ message: 'Failed to create country', error }, { status: response.status });
+            console.error(`Failed to create country: ${response.statusText}`);
+            throw new Error(response.statusText);
         }
     } catch (error) {
         console.error('Error creating country:', error);
